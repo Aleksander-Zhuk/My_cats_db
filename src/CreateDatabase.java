484f8ac645108edea798293f26f14712f169cf7b
Aleksander Zhuk <Zhuchko.s@gmail.com>
2024-01-09 01:35:55 +0300

    В My_cats.db добавили функцию insert_type

--- src/CreateDatabase.java
+++ src/CreateDatabase.java
@@ -1,23 +1,50 @@
 import java.sql.*;
+import java.util.Scanner;
+
 public class CreateDatabase {
     public static void main(String[] args) {
-
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/SQL/My_cats.db")) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("База данных создана.");
-            }
-            String sql = "CREATE TABLE IF NOT EXISTS types (\n"
-                    + " id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
-                    + " type VARCHAR(100) NOT NULL\n"
-                    + ");";
-            try (Statement stmt = conn.createStatement()) {
-                stmt.execute(sql);
-                System.out.println("Таблица успешно создана.");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        Scanner str = new Scanner(System.in);
+        My_cats my_cats = new My_cats();
+        for(int a = 0; a < 3; a++ ){
+        my_cats.insert_type(str.nextLine());}
     }
+}
+class My_cats {
+    boolean flag = true;
+        My_cats() {
+            if(flag){
+                try (Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/SQL/My_cats.db")) {
+                    if (conn != null) {
+                        DatabaseMetaData meta = conn.getMetaData();
+                        System.out.println("База данных создана.");
+                    }
+                    String sql = "CREATE TABLE IF NOT EXISTS types (\n"
+                        + " id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                        + " type VARCHAR(100) NOT NULL\n"
+                        + ");";
+                    try (Statement stmt = conn.createStatement()) {
+                        stmt.execute(sql);
+                        System.out.println("Таблица успешно создана.");
+                        flag = false;
+                    }
+                }
+                catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    public void insert_type(String type) {
+                try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:D:/SQL/My_cats.db");
+            String query = "INSERT INTO types (id, type) VALUES (?,?);";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(2, type);
+            int rowsInserted = statement.executeUpdate();
+            connection.close();
+        }
+                catch (SQLException e) {
+                    System.err.println(e.getMessage());
+                }
+        }
 }
 
